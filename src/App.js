45c0e5996892cb112fd337a3a6ba47cde380702a
MohdAdel <mohd.adel73@gmail.com>
2020-11-25 11:23:19 +0200

    establish enviroment

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import { Counter } from './features/counter/Counter';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Discord clone</h1>
+    </div>
+  );
+}
+
+export default App;
